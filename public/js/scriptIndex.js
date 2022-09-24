3b6d44e819a3038b50bd154e36d66bb524ac39b3
Hédi Lannoo <hedilannoo@gmail.com>
2022-09-24 20:51:07 +0200

    add images and script for button and description

--- public/js/scriptIndex.js
+++ public/js/scriptIndex.js
@@ -1,70 +1,106 @@
-
 let slide = document.querySelectorAll('.slide');
-let current = 0;
+let currentSlide = 0;
+let currentDescription = 0;
 let index = document.querySelector('.containerIndex');
-let para = document.querySelector('p');
+let para = document.querySelectorAll('p');
 
-function cls(){
-    for(let i = 0; i < slide.length; i++){
+function displayTrue() {
+    for (let i = 0; i < slide.length; i++)
+    {
         slide[i].style.display = 'none';
 
     }
 }
 
-function next(){
-    para.classList.add("lorem")
-    cls();
-    if(current === slide.length-1) current = -1;
-    current++;
-
-    slide[current].style.display = 'block';
-    slide[current].style.opacity = 0.4;
-
-
+function show() {
     let x = 0.4;
-    let intX = setInterval(function(){
-        x+=0.1;
-        slide[current].style.opacity = x;
-        if(x >= 1) {
+    let intX = setInterval(function () {
+        x += 0.1;
+        slide[currentSlide].style.opacity = x;
+        if (x >= 1) {
             clearInterval(intX);
             x = 0.4;
         }
     }, 100);
+}
+
+function displayDescription() {
+
+    for (let u = 0; u < para.length; u++) {
+        para[u].style.visibility = 'visible'
+        para[u].style.transform = 'translateY(0)'
+        para[u].style.transition = '5s'
+        para[currentDescription].style.opacity = '1'
+        para[u].classList.add("lorem");
+    }
+}
+
+
+function next() {
+    displayTrue();
+    displayDescription();
+
+    if (currentSlide === slide.length - 1) currentSlide = -1;
+    currentSlide++;
+
+    slide[currentSlide].style.display = 'block';
+    slide[currentSlide].style.opacity = 0.4;
+
+
+    if (currentDescription === para.length - 1) currentDescription = -1;
+    currentDescription++;
+
+    para[currentDescription].style.visibility = 'hidden';
+    para[currentDescription].style.transform = 'translateY(200px)'
+    para[currentDescription].style.opacity = '0'
+
+
+    show();
 
 }
 
-function prev(){
-    para.classList.add("lorem")
-    cls();
-    if(current === 0) current = slide.length;
-    current--;
+function prev() {
+    displayTrue();
+    displayDescription();
 
-    slide[current].style.display = 'block';
-    slide[current].style.opacity = 0.4;
+    if (currentSlide === 0) currentSlide = slide.length;
+    currentSlide--;
 
-    let x = 0.4;
-    let intX = setInterval(function(){
-        x+=0.1;
-        slide[current].style.opacity = x;
-        if(x >= 1) {
-            clearInterval(intX);
-            x = 0.4;
-        }
-    }, 100);
+    slide[currentSlide].style.display = 'block';
+    slide[currentSlide].style.opacity = 0.4;
+
+    if (currentDescription === 0) currentDescription = para.length;
+    currentDescription--;
+
+    para[currentDescription].style.visibility = 'hidden';
+    para[currentDescription].style.transform = 'translateY(200px)'
+    para[currentDescription].style.opacity = '0'
+
+
+    show();
 
 }
 
-function start(){
-    cls();
-    slide[current].style.display = 'block';
+function start() {
+    displayTrue();
+    slide[currentSlide].style.display = 'block';
+    para[currentDescription].style.visibility = 'hidden';
+    para[currentDescription].style.transform = 'translateY(200px)'
+    para[currentDescription].style.opacity = '0'
+    show();
 }
+
 start();
 
-// Script description
+let imageBox = document.querySelector('#test');
 
-document.addEventListener('mouseenter',() => {
-    para.classList.add("lorem")
-})
+    if (window.matchMedia("(min-width: 600px)").matches) {
+        console.log('test1')
+    } else {
+        console.log('test2')
+
+    }
+
 
 
 
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ScriptRepository;
+use MobileDetectBundle\DeviceDetector\MobileDetectorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
